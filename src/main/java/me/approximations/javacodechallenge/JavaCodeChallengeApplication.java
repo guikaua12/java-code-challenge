b578a83a26a8126a0c1fbaa6077cee50727c5d59
Approximations <datkidbosst@gmail.com>
2024-06-24 02:49:55 -0300

    Initial commit

---
+++ src/main/java/me/approximations/javacodechallenge/JavaCodeChallengeApplication.java
@@ -0,0 +1,13 @@
+package me.approximations.javacodechallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaCodeChallengeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaCodeChallengeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/approximations/javacodechallenge/javacodechallenge/JavaCodeChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package me.approximations.javacodechallenge.javacodechallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaCodeChallengeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
