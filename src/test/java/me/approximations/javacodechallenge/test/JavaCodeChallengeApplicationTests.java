db23469bc8b35fd50825e0a167f6596dd036525e
Approximations <datkidbosst@gmail.com>
2024-06-25 15:00:35 -0300

    feat: added base setup for tests

--- src/test/java/me/approximations/javacodechallenge/test/JavaCodeChallengeApplicationTests.java
+++ src/test/java/me/approximations/javacodechallenge/test/JavaCodeChallengeApplicationTests.java
@@ -1,4 +1,4 @@
-package me.approximations.javacodechallenge.javacodechallenge;
+package me.approximations.javacodechallenge.test;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
