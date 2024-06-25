e61f321efed2e17a22388d7870b11c7bde0a7960
Approximations <datkidbosst@gmail.com>
2024-06-24 21:56:33 -0300

    feat: secured UsuarioController#delete to ADMIN role

--- src/main/java/me/approximations/javacodechallenge/controllers/UsuarioController.java
+++ src/main/java/me/approximations/javacodechallenge/controllers/UsuarioController.java
@@ -4,6 +4,7 @@ import jakarta.validation.Valid;
 import me.approximations.javacodechallenge.dtos.*;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 public interface UsuarioController {
@@ -26,5 +27,6 @@ public interface UsuarioController {
     UsuarioDTO updatePassword(UpdateUsuarioPasswordDTO dto);
 
     @DeleteMapping("/{id}")
+    @PreAuthorize("hasAnyRole('ADMIN')")
     void delete(@PathVariable("id") Long id);
 }
