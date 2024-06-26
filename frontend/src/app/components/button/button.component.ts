4cdd3d062206f5ce46b6d188bfb6b8068dc93dac
Approximations <datkidbosst@gmail.com>
2024-06-26 17:51:42 -0300

    feat: added button variants

--- frontend/src/app/components/button/button.component.ts
+++ frontend/src/app/components/button/button.component.ts
@@ -3,6 +3,8 @@ import { twMerge } from 'tailwind-merge';
 import { NgClass } from '@angular/common';
 import { noop } from 'rxjs';
 
+type ButtonVariant = 'primary' | 'secondary' | 'danger';
+
 @Component({
   selector: 'app-button',
   standalone: true,
@@ -11,7 +13,7 @@ import { noop } from 'rxjs';
   ],
   template: `
     <button [type]="type" [disabled]="disabled"
-            [ngClass]="twMerge('w-full rounded-md wood bg-neutral-900 text-white px-2 py-1', ngClass)"
+            [ngClass]="twMerge('w-full flex items-center outline-0 gap-2 rounded-md wood text-white px-4 py-2', buttonVariants[variant], ngClass)"
             (click)="onClick()">
       <ng-content></ng-content>
     </button>
@@ -22,5 +24,12 @@ export class ButtonComponent {
   @Input() disabled? = false;
   @Input() ngClass = '';
   @Input() onClick: () => void = noop;
+  @Input() variant: ButtonVariant = 'primary';
+
   protected readonly twMerge = twMerge;
+  protected readonly buttonVariants: { [k in ButtonVariant]: string } = {
+    primary: 'text-sm hover:bg-zinc-700 bg-neutral-900',
+    secondary: 'bg-white border border-zinc-300 text-neutral-700 hover:bg-opacity-10',
+    danger: 'bg-red-200 text-red-600 hover:bg-red-300',
+  };
 }
